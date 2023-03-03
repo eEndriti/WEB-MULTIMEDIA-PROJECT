5070d080a077f8d13da61be03d6b2949018d327e
eEndriti <endritsadrijaj@gmail.com>
2023-03-03 23:20:49 +0100

    some changes to index and navbar,also added footer

--- modules/main-elements.js
+++ modules/main-elements.js
@@ -1,6 +1,6 @@
-export function getNavbar(div){
+export function getNavbar(div,element){
     let html = `
-            <nav class="navbar navbar-expand-lg navbar-light">
+            <nav class="navbar navbar-expand-lg ">
             <div class="container-fluid">
             <a class="navbar-brand" href="#">Logo</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -9,22 +9,38 @@ export function getNavbar(div){
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link active" href="/index.html">Home</a>
+                    <a class="nav-link " id="index" href="/index.html">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="./categories.html">Categories</a>
+                    <a class="nav-link " id="categories" href="./categories.html">Categories</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="./about.html">About</a>
+                    <a class="nav-link " id="post" href="./post.html">Post Animation</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="./contact.html">Contact</a>
+                    <a class="nav-link " id="about" href="./about.html">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link " id="contact" href="./contact.html">Contact</a>
                 </li>
                 </ul>
             </div>
             </div>
         </nav>
     `
-
+    document.getElementById(div).innerHTML = html
+    const el = document.getElementById(element)
+    el.classList.add('active')
+    
+}
+export function getFooter(div){
+    let html = ` <div class="content d-flex justify-content-between text-light">
+    <p class="p-2">All Rights Reserved &copy</p>
+    <ul class="d-flex justify-content-end">
+        <li class="p-2"><i class="fa-brands fa-facebook"></i></li>
+        <li class="p-2"><i class="fa-brands fa-instagram"></i></li>
+        <li class="p-2"><i class="fa-brands fa-twitter"></i></li>
+    </ul>
+</div>`
     document.getElementById(div).innerHTML = html
 }
